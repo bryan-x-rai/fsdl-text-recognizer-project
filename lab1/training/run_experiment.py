22bad592712ba7cef016be6022f1c19846c81c51
bryanxrai <bryanxrai@local>
2018-09-17 21:42:48 +0000

    be lab1 thing v3

--- lab1/training/run_experiment.py
+++ lab1/training/run_experiment.py
@@ -9,8 +9,8 @@ from training.util import train_model
 
 
 DEFAULT_TRAIN_ARGS = {
-    'batch_size': 128,
-    'epochs': 12
+    'batch_size': 256,
+    'epochs': 9
 }
 
 
@@ -30,7 +30,7 @@ def run_experiment(experiment_config: Dict, save_weights: bool, gpu_ind: int, us
         },
         "train_args": {
             "batch_size": 128,
-            "epochs": 10
+            "epochs": 9
         }
     }
     save_weights: if True, will save the final model weights to a canonical location (see Model in models/base.py)
