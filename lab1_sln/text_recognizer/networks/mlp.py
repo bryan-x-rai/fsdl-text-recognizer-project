56f205ab1e755f1ec30fbad257677d81ce092eb0
bryanxrai <bryanxrai@local>
2018-09-17 21:04:32 +0000

    my lab1 thing v2

--- lab1_sln/text_recognizer/networks/mlp.py
+++ lab1_sln/text_recognizer/networks/mlp.py
@@ -7,7 +7,7 @@ from tensorflow.keras.layers import Dense, Dropout, Flatten
 def mlp(input_shape: Tuple[int, ...],
         output_shape: Tuple[int, ...],
         layer_size: int=128,
-        dropout_amount: float=0.2,
+        dropout_amount: float=0.1,
         num_layers: int=3) -> Model:
     """
     Simple multi-layer perceptron: just fully-connected layers with dropout between them, with softmax predictions.
