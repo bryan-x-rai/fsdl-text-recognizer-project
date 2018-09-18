2b47f9864b9801b623e1532c678f24d78fcf67e9
bryanxrai <bryanxrai@local>
2018-09-18 00:01:33 +0000

    be lab1 thing vx

--- lab1/text_recognizer/networks/mlp.py
+++ lab1/text_recognizer/networks/mlp.py
@@ -1,18 +1,23 @@
 from typing import Tuple
 from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Dense, Dropout, Flatten
 
 def mlp(input_shape: Tuple[int, ...],
         output_shape: Tuple[int, ...],
         layer_size: int = 128,
+        dropout_amount: float = 0.07,
         num_layers: int = 3) -> Model:
     # Simple multi-layer perceptron: just fully-connected layers with softmax predictions; creates num_layers layers.
     
     num_classes = output_shape[0]
     model = Sequential()
     model.add(Flatten(input_shape = input_shape))
-    for _ in range(num_layers):
-        model.add(Dense(layer_size, activation = 'selu'))
+    # for _ in range(num_layers):
+    model.add(Dense(layer_size, activation = 'selu'))
+    model.add(Dense(layer_size, activation = 'selu'))
+    model.add(Dropout(dropout_amount))
+    model.add(Dense(layer_size, activation = 'selu'))
+    model.add(Dropout(dropout_amount))
     model.add(Dense(num_classes, activation = 'softmax'))
     
     return model
