f89677979b607961294642471c48e86a28b58b2d
bryanxrai <bryanxrai@local>
2018-09-18 01:14:02 +0000

    be lab1 MOAR tweaks 1

--- lab1/text_recognizer/networks/mlp.py
+++ lab1/text_recognizer/networks/mlp.py
@@ -5,12 +5,15 @@ from tensorflow.keras.layers import BatchNormalization, Dense, Dropout, Flatten
 def mlp(input_shape: Tuple[int, ...],
         output_shape: Tuple[int, ...],
         layer_size: int = 128,
-        dropout_amount: float = 0.223,
+        dropout_amount: float = 0.333,
         num_layers: int = 3) -> Model:
     # Simple multi-layer perceptron: just fully-connected layers with softmax predictions; creates num_layers layers.
     
     num_classes = output_shape[0]
     model = Sequential()
+    print ('Input shape [0] is...')
+    print (input_shape[0])
+    
     model.add(Flatten(input_shape = input_shape))
     # for _ in range(num_layers):
     model.add(BatchNormalization())
@@ -22,6 +25,8 @@ def mlp(input_shape: Tuple[int, ...],
     model.add(Dense(layer_size, activation = 'selu'))
     model.add(Dropout(dropout_amount))
     model.add(Dense(num_classes, activation = 'softmax'))
+    print ('I miss python 2... full plaid engage yet?')
+    model.summary()
     
     return model
 
