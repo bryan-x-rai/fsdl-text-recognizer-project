9faa3fef8f0d288777db7f121f41952c6971828b
bryanxrai <bryanxrai@local>
2018-09-18 03:39:08 +0000

    be lab1 MOAR tweaks

--- lab1/text_recognizer/networks/mlp.py
+++ lab1/text_recognizer/networks/mlp.py
@@ -5,28 +5,28 @@ from tensorflow.keras.layers import BatchNormalization, Dense, Dropout, Flatten
 def mlp(input_shape: Tuple[int, ...],
         output_shape: Tuple[int, ...],
         layer_size: int = 128,
-        dropout_amount: float = 0.333,
+        dropout_amount_1: float = 0,
+        dropout_amount_2: float = 0.1,
+        dropout_amount_3: float = 0.2,
         num_layers: int = 3) -> Model:
     # Simple multi-layer perceptron: just fully-connected layers with softmax predictions; creates num_layers layers.
     
     num_classes = output_shape[0]
     model = Sequential()
-    print ('Input shape [0] is...')
-    print (input_shape[0])
     
     model.add(Flatten(input_shape = input_shape))
-    # for _ in range(num_layers):
     model.add(BatchNormalization())
     model.add(Dense(layer_size, activation = 'selu'))
+    model.add(Dropout(dropout_amount_1))
     model.add(BatchNormalization())
     model.add(Dense(layer_size, activation = 'selu'))
-    # model.add(Dropout(dropout_amount))
+    model.add(Dropout(dropout_amount_2))
     model.add(BatchNormalization())
     model.add(Dense(layer_size, activation = 'selu'))
-    model.add(Dropout(dropout_amount))
+    model.add(Dropout(dropout_amount_3))
+    model.add(BatchNormalization())
     model.add(Dense(num_classes, activation = 'softmax'))
     print ('I miss python 2... full plaid engage yet?')
-    model.summary()
     
     return model
 
