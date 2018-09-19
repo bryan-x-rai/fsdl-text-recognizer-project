567762ceb3af7df5a564ffcfb2cad62bc5c34d10
bryanxrai <bryanxrai@local>
2018-09-19 00:59:41 +0000

    be lab2 thing vx2

--- lab2/text_recognizer/networks/lenet.py
+++ lab2/text_recognizer/networks/lenet.py
@@ -9,6 +9,19 @@ def lenet(input_shape: Tuple[int, ...],
          ) -> Model:
     num_classes = output_shape[0]
 
+    model = Sequential()
+    model.add(Lambda(lambda x: tf.expand_dims(x, -1), input_shape = input_shape))
+    model.add(Conv2D(32, (3, 3), activation = 'selu', input_shape = input_shape))
+    model.add(Conv2D(64, (3, 3), activation = 'selu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation = 'selu'))
+    model.add(Dense(num_classes, activation = 'softmax'))
+    
+    return model
+
+'''
+
     ##### Your code below (Lab 2)
     
     # "Your code"  ;)
@@ -16,17 +29,17 @@ def lenet(input_shape: Tuple[int, ...],
     model = Sequential()
     # model.add(Flatten(input_shape = input_shape))
     # model.add(Conv2D(32, (3, 3), activation = 'selu', input_shape = (28, 28, 1)))
-    # model.add(?)
     
-    if len(input_shape) < 3:
-        model.add(Lambda(lambda x: tf.expand_dims(x, -1), input_shape = input_shape))
-        input_shape = (input_shape[0], input_shape[1], 1)
+    # if len(input_shape) < 3:
+    
+    model.add(Lambda(lambda x: tf.expand_dims(x, -1), input_shape = input_shape))
+    # input_shape = (input_shape[0], input_shape[1], 1)
     
     model.add(Conv2D(32, (3, 3), activation = 'selu', input_shape = input_shape))
-    model.add(MaxPooling2D((2, 2)))
+    # model.add(MaxPooling2D((2, 2)))
     model.add(Conv2D(64, (3, 3), activation = 'selu'))
     model.add(MaxPooling2D((2, 2)))
-    model.add(Conv2D(64, (3, 3), activation = 'selu'))
+    # model.add(Conv2D(64, (3, 3), activation = 'selu'))
     model.add(Flatten())
     model.add(Dense(128, activation = 'selu'))
     model.add(Dense(num_classes, activation = 'softmax'))
@@ -34,3 +47,5 @@ def lenet(input_shape: Tuple[int, ...],
     ##### Your code above (Lab 2)
 
     return model
+
+'''
