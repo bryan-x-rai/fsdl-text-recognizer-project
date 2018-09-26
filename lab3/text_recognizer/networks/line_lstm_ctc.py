57f12651425b1a1a0deaa27cec151acaf9323291
bryanxrai <bryanxrai@local>
2018-09-26 02:10:01 +0000

    be lab3 4

--- lab3/text_recognizer/networks/line_lstm_ctc.py
+++ lab3/text_recognizer/networks/line_lstm_ctc.py
@@ -47,7 +47,7 @@ def line_lstm_ctc(input_shape, output_shape, window_width=28, window_stride=14):
     convnet = KerasModel(inputs = convnet.inputs, outputs = convnet.layers[-2].output)
     convnet_outputs = TimeDistributed(convnet)(image_patches)
     
-    lstm_output = lstm_fn(128, return_sequences = True)(convnet_outputs)
+    lstm_output = Bidirectional(lstm_fn(128, return_sequences = True))(convnet_outputs)
     
     softmax_output = Dense(num_classes, activation = 'softmax', name = 'softmax_output')(lstm_output)
 
