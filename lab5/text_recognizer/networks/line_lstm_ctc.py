c1a5de5f5e998204fcacf113fee7703742fb8cac
bryanxrai <bryanxrai@local>
2018-10-01 20:23:53 +0000

    be lab5 2

--- lab5/text_recognizer/networks/line_lstm_ctc.py
+++ lab5/text_recognizer/networks/line_lstm_ctc.py
@@ -80,6 +80,8 @@ def line_lstm_ctc(input_shape, output_shape, window_width=28, window_stride=14):
     
     softmax_output = Dense(num_classes, activation = 'softmax', name = 'softmax_output')(lstm3_output)
     
+    # Test evaluation: 0.612532686461871
+    
     '''
     image_reshaped = Reshape((image_height, image_width, 1))(image_input)
     # (image_height, image_width, 1)
